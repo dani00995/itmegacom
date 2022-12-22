8f7e248e7cf9dbaabd09ba6e020a8924026a09e2
bigli <daniel.kg9526@gmail.com>
2022-12-23 04:35:49 +0600

    finish

--- lib/lesson_10_home_work.dart
+++ lib/lesson_10_home_work.dart
@@ -13,6 +13,7 @@ void main(List<String> args) {
 //Сын унаследовал от отца цвет волос и глаз, но внешность больше похожа на
 //славянскую, как у матери. Сын также является левшой. Создайте класс отца(Father)
 //и наследующий от него класс сына(Son).
+  Son().getDetiles();
 
 // 3)Машина Toyota имеет 3 поколения машин.
 
@@ -21,6 +22,12 @@ void main(List<String> args) {
 // Во втором поколении поменяли мотор на 'GR', подвеска стала пневмоподвеской.
 // В третьем поколении поменяли фары на лезерные и кузов состоит из металозаменителя.
 // Вывыдите на экран все поколения с параметрами.
+  Toyota generationOne = Toyota();
+  generationOne.printDetile();
+  ToyotaTwo generationTwo = ToyotaTwo();
+  generationTwo.printDetile();
+  ToyotaThree generationThree = ToyotaThree();
+  generationThree.printDetile();
 }
 
 class Card {
@@ -63,12 +70,61 @@ class Valet extends Card {
     super.printCard('Валет');
   }
 }
-//_________________________________task 3 _____________
 
+//========// task 2 //=========//
 class Father {
-  String ruka = 'правша';
-  String volosy = 'брюнет';
-  String glaza = 'карие';
-  String vneshnost = 'Азиат';
-  Father({String? ruka, String? volosy, String? glaza, String? vneshnost});
+  String hand = 'правша';
+  String hair = 'брюнет';
+  String eyes = 'карие';
+  String appearance = 'Азиатская';
+}
+
+class Son extends Father {
+  @override
+  String get appearance => super.appearance = "Славянская";
+  @override
+  String get hand => super.hand = "левша";
+  void getDetiles() {
+    print(
+        "Внешность: $appearance, \nВолосы: $hair, \nГлаза: $eyes, \nРука: $hand");
+  }
+}
+
+class Toyota {
+  int generation;
+  String engine;
+  String body;
+  String suspension;
+  String headlights;
+  Toyota(
+      {this.generation = 1,
+      this.engine = "JZ",
+      this.body = "железный",
+      this.suspension = "обычная",
+      this.headlights = "ксеноновые"});
+  void printDetile() {
+    print(
+        "\nПоколение: $generation\nдвигатель: $engine,\nкузов машины: $body,\nподвеска: $suspension,\nфары: $headlights");
+  }
+}
+
+class ToyotaTwo extends Toyota {
+  @override
+
+  int get generation => super.generation=2;
+  @override
+  String get engine => super.engine = "GR";
+  @override
+  String get suspension => super.suspension = "пневмоподвеска";
+  
+}
+
+class ToyotaThree extends ToyotaTwo {
+  @override
+  int get generation => super.generation=3;
+  @override
+  String get headlights => super.headlights = "лезерные";
+  @override
+  String get body => super.body = "металозаменитель";
+  
 }
